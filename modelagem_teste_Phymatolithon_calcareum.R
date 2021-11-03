30a3dfa1c38332516a21800f971fe1917d36f71b
guilhermechicarolli <gui.chicarolli@outlook.com>
2021-11-02 23:05:52 -0300

    script teste de modelagem

--- modelagem_teste_Phymatolithon_calcareum.R
+++ modelagem_teste_Phymatolithon_calcareum.R
@@ -1,12 +1,13 @@
 ############# SCRIPT DO PROJETO DE MODELAGEM DE RODOLITOS #############
 
-# 1. Modelagem teste de Phymatolithon calcareum
+# 1. Teste VIF com as variáveis
+# 2. Modelagem teste de Phymatolithon calcareum
 
 ##########################################################################################
 
 # Carregar as bibliotecas necessarias 
 
-if (!require(dplyr)) install.packages('dplyr')
+if (!require(dplyr)) install.packages('tidyverse')
 if (!require(ggplot2)) install.packages('ggplot2')
 if (!require(grDevices)) install.packages('grDevices')
 if (!require(sf)) install.packages('sf')
@@ -18,13 +19,15 @@ if (!require(rgdal)) install.packages('rgdal')
 if (!require(ggsn)) install.packages('ggsn')
 if (!require(sdmpredictors)) install.packages('sdmpredictors')
 if (!require(leaflet)) install.packages('leaflet')
+if (!require(RStoolbox)) install.packages('RStoolbox')
+if (!require(usdm)) install.packages('usdm')
+if (!require(sp)) install.packages('sp')
 
 
 ##########################################################################################
 
 ### CARREGAR A PLANILHA
 rod <- read.csv('Banco_de_dados_Rodolitos.csv', sep=';')
-rod <-as_tibble(rod)
 
 # Mudar a separação decimal das colunas de lat e long de virgula para ponto
 rod$Lat <- scan(text=rod$Lat, dec=",", sep=".")
@@ -44,7 +47,8 @@ length(rod$Lat)
 # Carregar dados de ocorrencia de Phymatolithon calcareum
 calc <- rod %>%
     filter(rod$Spp == "Phymatolithon calcareum") %>%
-    select(Lat, Long)
+    dplyr::select(Lat, Long) %>%
+    drop_na()
 
 
 ##########################################################################################
@@ -75,7 +79,71 @@ cams_26 <- raster::stack(cams_26)
 plot(cams_26[[1]])  # velocidade da corrente na superfície
 
 
+##########################################################################################
 
+#------- Teste VIF com as variáveis ambientais -------#
+
+# Permite que dados espaciais sejam associados com o sistema de coordenadas,
+# criando uma projeção que pode ser utilizada nos rasters
+proj_WGS <- sp::CRS(
+    "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+
+# Converção dos pontos geográficos para SpatialPoints
+sp::coordinates(calc) <- Long~Lat
+
+# Adicionar a projeção
+raster::crs(calc) <- proj_WGS
+
+# Verificação dos dados
+pontos_planta
+
+vif(cams_pres)
+
+
+ex <- raster::extract(cams_pres, calc)
+head(ex)
+
+
+vif <- usdm::vifstep(ex)
+cor(ex)
+
+
+##########################################################################################
+
+
+#--------- 2. RODAGEM DO MODELO PREVIO E SELECAO DAS VARIAVEIS   ---------#
+
+# MODELO CHEIO
+
+calc$species <- 1
+
+# Adicionar os dados previo para fazer um modelo com todas as variaveis
+dados_mod_cheio <- sdm::sdmData(species~., spg, predictors = bioCams, 
+                   bg=list(method='gRandom', n=10000))
+dados_mod_cheio
+
+
+# Nome dos algoritmos
+getmethodNames()
+
+# Ajustar e criar os modelos
+modeloC <- sdm::sdm(species~., dados_mod_cheio, methods = c('maxent','rf','glm','maxlike',
+                                                            'gam','bioclim','domain.dismo'), replication=c('sub', 'boot'),
+               test.p=30, n=25, parallelSettings=list(ncore=5, method='parallel'))
+
+# NOTAS: 1) Para o MaxEnt funcionar o Java do computador deve estar atualizado. 
+# 2) O parametro ncore e a quantidade de cores de processamento utilizados para
+# a modelagem, altere conforme a capacidade do computador
+
+modeloC
+
+# Plot da importancia das variaveis
+plot(getVarImp(modeloC), 'AUC', main="Importância relativa das biovariáveis", 
+     ylab='Variáveis', xlab="Importância relativa da variável") # Biovars: 6, 17, 19, 14
+getVarImp(modeloC)
+
+# Para abrir uma interface de exploracao do modelo
+sdm::gui(modeloC)
 
 
 
